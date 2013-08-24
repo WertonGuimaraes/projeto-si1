9575e856f9126efd1f1a9b1a63fb4aec859c3c6a
tiaraju <tiaraju.smaneoto@ccc.ufcg.edu.br>
2013-08-24 17:16:08 -0300

    US1

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,16 +1,58 @@
 package model;
 
+import java.security.InvalidParameterException;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Usuario {
 	
 	private String login,nome,email,endereco,telefone;
+	private Set<Usuario> friends;
 	
-	public Usuario(String login, String nome, String email,String endereco, String telefone) {
+	public Usuario(String login, String nome, String email,String endereco, String telefone) throws InvalidParameterException {
+		if(login == null || nome==null || email ==null || endereco ==null || telefone ==null){
+			throw new InvalidParameterException("parametro(s) nulos");
+		}
+		if(isEmpty(login)|| isEmpty(nome) || isEmpty(email) || isEmpty(telefone) || isEmpty(endereco)){
+			throw new InvalidParameterException("parametro(s) vazios");
+		}
+		
 		this.login=login;
 		this.nome=nome;
 		this.email=email;
 		this.endereco=endereco;
 		this.telefone=telefone;
+		this.friends= new HashSet<Usuario>();
 		
+	}
+	
+	public boolean isFriendOf(Usuario user){
+		return friends.contains(user);
+	}
+	
+	public void addFriend(Usuario user) {
+		friends.add(user);
+	}
+	
+	@Override
+	public boolean equals(Object obj){
+		if(!(obj instanceof Usuario)){
+			return false;
+		}
+		Usuario user = (Usuario) obj;
+		return (user.getLogin().equals(this.getLogin()));
+	}
+	
+	public Set<Usuario> getFriends() {
+		return friends;
+	}
+
+	public void setFriends(Set<Usuario> friends) {
+		this.friends = friends;
+	}
+
+	private boolean isEmpty(String s){
+		return s.equals("");
 	}
 	
 	public String getLogin() {
--- src/testes/UserTest.java
+++ src/testes/UserTest.java
@@ -2,15 +2,47 @@ package testes;
 
 import model.Usuario;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class UserTest {
 
-	@Test
-	public void test() {
-		Usuario user1 = new Usuario("tiaraju","Tiaraju","tiarajums@gmail.com","rua xxx numero 222","88888888");
-		Usuario user2 = new Usuario("crafs","Carlos Rafael","crafs@gmail.com","rua yyy numero 333","22222222");
-		
+	Usuario user1,user2,user3;
+
+	@Before
+	public void init(){
+		user1 = new Usuario("tiaraju","Tiaraju","tiarajums@gmail.com","rua xxx numero 222","88888888");
+		user2 = new Usuario("crafs","Carlos Rafael","crafs@gmail.com","rua yyy numero 333","22222222");
+		user3 = new Usuario("jessika","Jessika","reh.nally@gmail.com","rua www numero 123","99999999");
 	}
+
+	@Test
+	public void testLoginNull() {
+		try{
+			user3 = new Usuario(null,"Carlos Rafael","crafs@gmail.com","rua yyy numero 333","22222222");
+			Assert.fail();
+		}catch(Exception e){}
+
+	}
+
+	@Test
+	public void testUserNull() {
+		user1 = new Usuario("tiaraju","Tiaraju","tiarajums@gmail.com","rua xxx numero 222","88888888");
+		user2 = new Usuario("crafs","Carlos Rafael","crafs@gmail.com","rua yyy numero 333","22222222");
+		try{
+			user3 = new Usuario("rafael",null,"crafs@gmail.com","rua yyy numero 333","22222222");
+			Assert.fail();
+		}catch(Exception e){}
+
+	}
+
+	@Test
+	public void testAddFriend(){
+		user1.addFriend(user2);
+		Assert.assertTrue(user1.isFriendOf(user2));
+	}
+	
+	
 
 }
