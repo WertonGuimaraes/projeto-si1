fdec34cc21096c0908c4223998d8d6799d908ca4
tiaraju <tiaraju.smaneoto@ccc.ufcg.edu.br>
2013-08-30 00:21:00 -0300

    parse to list

--- src/model/Util.java
+++ src/model/Util.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.List;
+
 public class Util {
 
 	
@@ -10,4 +12,12 @@ public class Util {
 	public static boolean isEmpty(String s){
 		return s.equals("");
 	}
+	
+	
+	public static String easyAccListParser(List<?> list){
+		String result = list.toString();
+		return "{"+result.substring(1,result.length()-1)+"}";
+	}
+	
+	
 }
