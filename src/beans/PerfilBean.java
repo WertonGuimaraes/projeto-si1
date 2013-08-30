0764f86d0048907f25742004c80abdce7f0e3e3e
jessikarenally <reh.nally@gmail.com>
2013-08-30 11:30:46 -0300

    Tornando a versao de loginbean compativel com nova versao de id de usuari, antes inteiro agora string

--- src/beans/PerfilBean.java
+++ src/beans/PerfilBean.java
@@ -1,10 +1,13 @@
 package beans;
 
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import model.Carona;
 import model.Usuario;
 import controller.Controller;
 import controller.SessionController;
@@ -12,10 +15,9 @@ import controller.SessionController;
 @SessionScoped
 @ManagedBean(name="perfilBean")
 public class PerfilBean {
-	public static int ID;
-	private  int id;
-	private  Usuario usuario;	 
-
+	public static String ID;
+	private String id;
+	private  Usuario usuario;	
 	
 	public Usuario getUsuario() {
 		return usuario;
@@ -24,9 +26,10 @@ public class PerfilBean {
 	public PerfilBean(){
 		System.out.println("no construtorp");
 		System.out.println(ID);
-		if(ID != 0){
+		if(ID != null){
 			this.usuario = SessionController.getInstance().searchSessionById(ID);
 			this.id = ID;
+			
 		}
 		
 		this.usuario = SessionController.getInstance().searchSessionById(ID);
@@ -34,6 +37,15 @@ public class PerfilBean {
 	
 	public void test(){
 		System.out.println("iai?");
+	}
+	
+	public String logoffButton(){
+		SessionController.getInstance().desconectarSessao(0); //FIXME o certo e passar id
+		return "index.xhtml";
+	}
+	
+	public Collection<Carona> getCaronas(){
+		return usuario.getCaronas().values();
 	}
 	
 
