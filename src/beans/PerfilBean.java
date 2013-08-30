9c5796018aa8374e4469848472ee8be9ada632d3
jessikarenally <reh.nally@gmail.com>
2013-08-30 15:02:44 -0300

    mudei algumas coisas em perfilbean e perfil

--- src/beans/PerfilBean.java
+++ src/beans/PerfilBean.java
@@ -6,19 +6,24 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 
 import model.Carona;
 import model.Usuario;
 import controller.Controller;
 import controller.SessionController;
 
-@SessionScoped
+@ViewScoped
 @ManagedBean(name="perfilBean")
 public class PerfilBean {
 	public static String ID;
 	private String id;
+	String origem;
+	String destino;
 	private  Usuario usuario;	
+	private List<Carona> caronasDisponiveis;
 	
+
 	public Usuario getUsuario() {
 		return usuario;
 	}
@@ -26,13 +31,13 @@ public class PerfilBean {
 	public PerfilBean(){
 		System.out.println("no construtorp");
 		System.out.println(ID);
+	
 		if(ID != null){
 			this.usuario = SessionController.getInstance().searchSessionById(ID);
 			this.id = ID;
-			
+					
 		}
-		
-		this.usuario = SessionController.getInstance().searchSessionById(ID);
+		else throw new RuntimeException("Erro no perfil");
 	}
 	
 	public void test(){
@@ -46,6 +51,43 @@ public class PerfilBean {
 	
 	public Collection<Carona> getCaronas(){
 		return usuario.getCaronas().values();
+	}
+	
+	public void localizaCaronasDisponiveis(){
+		List<Integer> idsCaronas = Controller.getInstance().buscaCarona(this.origem, this.destino);
+		
+		for(Integer id: idsCaronas){
+			this.caronasDisponiveis.add(Controller.getInstance().buscaCarona(id));
+		}
+		
+	}
+	
+	public void cadastraNovaCarona(){
+		
+	}
+
+	public String getOrigem() {
+		return origem;
+	}
+
+	public void setOrigem(String origem) {
+		this.origem = origem;
+	}
+
+	public String getDestino() {
+		return destino;
+	}
+
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
+	
+	public List<Carona> getCaronasDisponiveis() {
+		return caronasDisponiveis;
+	}
+	
+	public void setCaronasDisponiveis(List<Carona> caronasDisponiveis) {
+		this.caronasDisponiveis = caronasDisponiveis;
 	}
 	
 
