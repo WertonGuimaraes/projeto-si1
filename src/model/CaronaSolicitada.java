ce109b04cfb98b1a877baffabebc97371803d46d
jessikarenally <reh.nally@gmail>
2013-09-05 16:05:58 -0300

    mudanca minima, mas vou dar push

--- src/model/CaronaSolicitada.java
+++ src/model/CaronaSolicitada.java
@@ -1,12 +1,16 @@
 package model;
 
+import controller.Controller;
+
 public class CaronaSolicitada {
 	private Carona carona;
+	private int idCarona;
 	private String pontoDeEncontro;
 	//private CaronaState state;
 	
-	public CaronaSolicitada(Carona carona, String pontoDeEncontro){
-		this.carona = carona;
+	public CaronaSolicitada(int idCarona, String pontoDeEncontro){
+		this.carona = Controller.getInstance().buscaCarona(idCarona);
+		this.idCarona = idCarona;
 		this.pontoDeEncontro = pontoDeEncontro;
 		//this.state = 
 	}
