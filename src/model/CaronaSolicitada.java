41f1f9026a38f08ceebefcab925436de6a6da6b8
jessikarenally <reh.nally@gmail>
2013-09-05 16:12:38 -0300

    colocando as classes da maquina de estado de carona 2

--- src/model/CaronaSolicitada.java
+++ src/model/CaronaSolicitada.java
@@ -1,18 +1,37 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import controller.Controller;
 
 public class CaronaSolicitada {
 	private Carona carona;
 	private int idCarona;
-	private String pontoDeEncontro;
+	private List<String> pontosDeEncontro;
 	//private CaronaState state;
 	
 	public CaronaSolicitada(int idCarona, String pontoDeEncontro){
 		this.carona = Controller.getInstance().buscaCarona(idCarona);
 		this.idCarona = idCarona;
-		this.pontoDeEncontro = pontoDeEncontro;
-		//this.state = 
+		this.pontosDeEncontro = returnPontos(pontoDeEncontro);
+		this.state = new SugerirPontosDeEncontro(this, pontosDeEncontro); 
+	}
+	
+	
+	public List<String> returnPontos(String pontos){
+		List<String> listPontos = new ArrayList<String>();
+		
+		for(String ponto: pontos.split(";")){
+			listPontos.add(ponto);
+		}
+		
+		return listPontos;
+	}
+	
+	public void responderSugestaoPontoDeEncontro(List<String> pontosConvenientes){
+		state = new RespostaAosPontosSugeridos(this, pontosConvenientes);
+		
 	}
 
 }
