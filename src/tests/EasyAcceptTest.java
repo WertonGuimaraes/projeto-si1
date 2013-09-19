95164d02bc40d62d34874bd2f137a843fabeb71f
jessikarenally <reh.nally@gmail.com>
2013-09-19 07:58:24 -0300

    fix

--- src/tests/EasyAcceptTest.java
+++ src/tests/EasyAcceptTest.java
@@ -15,17 +15,10 @@ public class EasyAcceptTest {
 		files.add("us/US02.txt");
 		files.add("us/US03.txt");
 		files.add("us/US04.txt");
-<<<<<<< HEAD
 		files.add("us/US05.txt");
-		// files.add("us/US06.txt");
-		// files.add("us/US07.txt");
-		// files.add("us/US08.txt");
-=======
-		files.add("us/US05.txt"); // ultima US da primeira entrega =p
 		files.add("us/US06.txt");
 		files.add("us/US07.txt");
 		files.add("us/US08.txt");// ultima US da segunda entrega =p
->>>>>>> 4c3a2a8043c9a3572194a2d2cb5590f1be016354
 		// files.add("us/US09.txt");
 		// files.add("us/US10.txt");
 		// files.add("us/US11.txt");
