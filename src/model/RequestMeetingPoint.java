664651657d66242a3f19e6720993bbf40f57aab9
jessikarenally <reh.nally@gmail.com>
2013-09-19 21:39:56 -0300

    colocando javadoc

--- src/model/RequestMeetingPoint.java
+++ src/model/RequestMeetingPoint.java
@@ -8,6 +8,12 @@ import controller.Controller;
 import model.state.AguardandoAceitacao;
 import model.state.StateChangeRequest;
 
+/***
+ * Classe responsável por criar uma requisição
+ * de ponto de encontro
+ * @author Jessika, Tiaraju, Rafael e Werton
+ *
+ */
 public class RequestMeetingPoint implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -18,57 +24,86 @@ public class RequestMeetingPoint implements Serializable {
 	private String idSessaoCaroneiro;
 	private String idSessaoMotorista;
 	private StateChangeRequest state;
-
+	
+	/**
+	 * Construtor de uma requisicao de ponto de encontro
+	 * @param idCarona  id da carona desejada
+	 * @param idSessaoCaroneiro id da sessao do caroneiro
+	 * @param pontosDeEncontro pontos de encontro
+	 */
 	public RequestMeetingPoint(int idCarona, String idSessaoCaroneiro,
 			String pontosDeEncontro) {
 		this.idCarona = idCarona;
 		this.idSessaoCaroneiro = idSessaoCaroneiro;
-		this.pontosCaroneiro = extractListPoints(pontosDeEncontro);
+		this.pontosCaroneiro = Util.extractListPoints(pontosDeEncontro);
 		this.state = new AguardandoAceitacao(this);
 	}
 	
-	private List<String> extractListPoints(String pontosDeEncontro) {
-		List<String> result = new ArrayList<String>();
-		
-		for(String ponto: pontosDeEncontro.split(";")){
-			result.add(ponto);
-		}
-		
-		return result;
-	}
 
 	public int getIdSugestao() {
 		return idSugestao;
 	}
-
+	
+	/**
+	 * Método acessador para o estado da sugestão
+	 * @return um objeto StateChangeRequest
+	 */
 	public StateChangeRequest getState() {
 		return state;
 	}
 
+	/***
+	 * Método acessador dos pontos do caroneiro
+	 * @return lista dos pontos do caroneiro
+	 */
 	public List<String> getPontosCaroneiro() {
 		return pontosCaroneiro;
 	}
 
+	/***
+	 * Método acessador dos pontos do motorista
+	 * @return lista dos pontos do motorista
+	 */
 	public List<String> getPontosMotorista() {
 		return pontosMotorista;
 	}
 
+	/**
+	 * Método acessador para o id da carona
+	 * @return id da carona
+	 */
 	public int getIdCarona() {
 		return idCarona;
 	}
 
+	/***
+	 * Método acessador para o id da sessao do caroneiro
+	 * @return id da sessao do caroneiro
+	 */ 
 	public String getIdSessaoCaroneiro() {
 		return idSessaoCaroneiro;
 	}
 
+	/***
+	 * Método acessador para o id da sessao do motorista
+	 * @return id da sessao do motorista
+	 */
 	public String getIdSessaoMotorista() {
 		return idSessaoMotorista;
 	}
-
+	
+	/***
+	 * Método modificador para o id da sugestão
+	 * @param idSugestao
+	 */
 	public void setIdSugestao(int idSugestao) {
 		this.idSugestao = idSugestao;
 	}
 
+	/**
+	 * Modifica o estado da carona
+	 * @param state
+	 */
 	public void setState(StateChangeRequest state) {
 		this.state = state;
 	}
--- src/model/ResponseMeetingPoint.java
+++ src/model/ResponseMeetingPoint.java
@@ -9,6 +9,12 @@ import model.state.RequisicaoRecebida;
 import model.state.StateChangeRequest;
 import model.state.StateChangeResponse;
 
+/**
+ * Classe responsavel pela resposta a uma sugestão de ponto 
+ * de encontro
+ * @author Jessika, Tiaraju, Rafael e Werton
+ *
+ */
 public class ResponseMeetingPoint implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -20,6 +26,12 @@ public class ResponseMeetingPoint implements Serializable {
 	private String idSessaoMotorista;
 	private StateChangeResponse state;
 	
+	/**
+	 * Construtor da resposta à sugestão de ponto de encontro
+	 * @param idCarona 
+	 * @param idSessaoCaroneiro 
+	 * @param pontosDeEncontro
+	 */
 	public ResponseMeetingPoint(int idCarona, String idSessaoCaroneiro,
 			String pontosDeEncontro) {
 		this.idCarona = idCarona;
@@ -47,34 +59,67 @@ public class ResponseMeetingPoint implements Serializable {
 		return state;
 	}
 
+	/***
+	 * Método acessador dos pontos do caroneiro
+	 * @return lista dos pontos do caroneiro
+	 */
 	public List<String> getPontosCaroneiro() {
 		return pontosCaroneiro;
 	}
 
+	
+	/***
+	 * Método acessador dos pontos do motorista
+	 * @return lista dos pontos do motorista
+	 */
 	public List<String> getPontosMotorista() {
 		return pontosMotorista;
 	}
-
+	
+	/**
+	 * Método acessador para o id da carona
+	 * @return id da carona
+	 */
 	public int getIdCarona() {
 		return idCarona;
 	}
 
+	/***
+	 * Método acessador para o id da sessao do caroneiro
+	 * @return id da sessao do caroneiro
+	 */ 
 	public String getIdSessaoCaroneiro() {
 		return idSessaoCaroneiro;
 	}
 
+	/***
+	 * Método acessador para o id da sessao do motorista
+	 * @return id da sessao do motorista
+	 */
 	public String getIdSessaoMotorista() {
 		return idSessaoMotorista;
 	}
 
+	/***
+	 * Método modificador para o id da sugestão
+	 * @param idSugestao
+	 */
 	public void setIdSugestao(int idSugestao) {
 		this.idSugestao = idSugestao;
 	}
 
+	/**
+	 * Modifica o estado da carona
+	 * @param state
+	 */
 	public void setState(StateChangeResponse state) {
 		this.state = state;
 	}
 
+	/***
+	 * Permite ao motorista aprovar o a solicitação e sugerir pontos
+	 * @param pontos
+	 */
 	public void aceitaSolicitacao(String pontos) {
 		this.state.aceitaSolicitacao(this, pontos);
 	}
